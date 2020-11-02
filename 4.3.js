a6eb8a9fb50f3f18f31c43855be423d7567abc60
cemil <cemilyilmaz.be@gmail.com>
2020-11-02 18:56:16 +0100

    testAli

--- 4.3.js
+++ 4.3.js
@@ -1,11 +1,16 @@
-let array = [1];
+let array = [];
 
-function rand10(max) {
-    return Math.floor(Math.random()*Math.floor(10+1));
+function rand10() {
+    let nbr = Math.floor(Math.random()*5);
+    return nbr;
 }
 function multiRand(n) {
     for (let i = 0; i < n; i++) {
         array.push(rand10());
     }
+    return array;
 }
+
+let n = 5;
+multiRand(n);
 console.log(array);
